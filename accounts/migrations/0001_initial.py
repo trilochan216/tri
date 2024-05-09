1f2dcff7faddf0aa10d5ff269aac70f95761e44f
suman01 <sumanpaudel2004@gmail.com>
2024-05-09 17:00:10 +0545

    profile section create by suman paudel

--- accounts/migrations/0001_initial.py
+++ accounts/migrations/0001_initial.py
@@ -20,10 +20,11 @@ class Migration(migrations.Migration):
             fields=[
                 ('uid', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
                 ('is_email_verified', models.CharField(blank=True, max_length=100, null=True)),
                 ('profile_image', models.ImageField(upload_to='profile')),
                 ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='profile', to=settings.AUTH_USER_MODEL)),
+
             ],
             options={
                 'abstract': False,
